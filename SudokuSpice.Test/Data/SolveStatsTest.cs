bbd0383708a2cc032141b94d5daf573d36edcfa9
Morgan Roff <roffmorgan@gmail.com>
2020-03-01 18:41:32 -0800

    Create SolveStats for tracking stats during solve attempts

---
+++ SudokuSpice.Test/Data/SolveStatsTest.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace SudokuSpice.Data.Test
+{
+    public class SolveStatsTest
+    {
+        [Theory]
+        [MemberData(nameof(EqualSolveStats))]
+        public void Equals_WithEqualSolveStats_IsTrue(SolveStats a, SolveStats b)
+        {
+            Assert.Equal(a, b);
+            Assert.True(a.Equals(b));
+            Assert.True(a == b);
+            Assert.False(a != b);
+        }
+
+        public static IEnumerable<object[]> EqualSolveStats()
+        {
+            yield return new object[]
+            {
+                new SolveStats(),
+                new SolveStats(),
+            };
+            yield return new object[]
+            {
+                new SolveStats() {NumSolutionsFound = 1, NumSquaresGuessed = 2, NumTotalGuesses = 3},
+                new SolveStats() {NumSolutionsFound = 1, NumSquaresGuessed = 2, NumTotalGuesses = 3},
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(UnequalSolveStats))]
+        public void Equals_WithUnequalSolveStats_IsFalse(SolveStats a, SolveStats b)
+        {
+            Assert.NotEqual(a, b);
+            Assert.False(a.Equals(b));
+            Assert.False(a == b);
+            Assert.True(a != b);
+        }
+
+        public static IEnumerable<object[]> UnequalSolveStats()
+        {
+            yield return new object[]
+            {
+                new SolveStats(),
+                new SolveStats() { NumSolutionsFound = 1},
+            };
+            yield return new object[]
+            {
+                new SolveStats(),
+                new SolveStats() { NumSquaresGuessed = 1 },
+            };
+            yield return new object[]
+            {
+                new SolveStats(),
+                new SolveStats() { NumTotalGuesses = 1 },
+            };
+            yield return new object[]
+            {
+                new SolveStats() { NumSolutionsFound = 1, NumSquaresGuessed = 3, NumTotalGuesses = 5 },
+                new SolveStats() { NumSolutionsFound = 10, NumSquaresGuessed = 30, NumTotalGuesses = 50 },
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(SolveStatsAndObjectsForEquality))]
+        public void Equals_WithObject_IsCorrect(SolveStats stats, object other, bool isEqual)
+        {
+            Assert.Equal(isEqual, stats.Equals(other));
+        }
+
+        public static IEnumerable<object[]> SolveStatsAndObjectsForEquality()
+        {
+            yield return new object[]
+            {
+                new SolveStats(),
+                new SolveStats(),
+                true,
+            };
+            yield return new object[]
+            {
+                new SolveStats(),
+                new BitVector(),
+                false,
+            };
+            yield return new object[]
+            {
+                new SolveStats(),
+                null,
+                false,
+            };
+            yield return new object[]
+            {
+                new SolveStats(),
+                new List<SolveStats>(),
+                false,
+            };
+        }
+    }
+}
---
+++ SudokuSpice/Data/SolveStats.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace SudokuSpice.Data
+{
+    /// <summary>
+    /// Contains statistics about an attempt to solve a puzzle.
+    /// </summary>
+    public struct SolveStats : IEquatable<SolveStats>
+    {
+        /// <summary>
+        /// The number of solutions found for this puzzle. For any real Sudoku puzzle, this should
+        /// be 1.
+        /// </summary>
+        public int NumSolutionsFound { get; set; }
+        /// <summary>
+        /// The number of squares for which the solver had to guess while finding a solution.
+        /// </summary>
+        /// <remarks>
+        /// Unlike <c>NumTotalGuesses</c>, if the solver was setting a squar that had three
+        /// possible values, this would only add 1 to <c>NumSquaresGuessed</c>. 
+        /// If NumSolutionsFound is greater than 1, then the precise meaning of this value is 
+        /// undefined.
+        /// </remarks>
+        public int NumSquaresGuessed { get; set; }
+        /// <summary>
+        /// The total number of guesses used to solve the puzzle. A 'guess' is any time the
+        /// solver tries setting a square to one value when there are multiple possible values
+        /// for that square.
+        /// </summary>
+        /// <remarks>
+        /// Example Guesses: if the solver tried to set square (0,1) with possible values [2, 3, 5]
+        /// then this would add three guesses to the puzzle solve since there were three possible
+        /// values for this coordinate. This would be true even if only one of the values actually
+        /// led to a solution.
+        /// </remarks>
+        public int NumTotalGuesses { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is SolveStats other)
+            {
+                return Equals(other);
+            }
+            return false;
+        }
+
+        public bool Equals(SolveStats other)
+        {
+            return NumSolutionsFound == other.NumSolutionsFound
+                && NumSquaresGuessed == other.NumSquaresGuessed
+                && NumTotalGuesses == other.NumTotalGuesses;
+        }
+
+        public override int GetHashCode()
+        {
+            return NumSolutionsFound ^ NumSquaresGuessed ^ NumTotalGuesses;
+        }
+
+        public static bool operator ==(SolveStats left, SolveStats right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(SolveStats left, SolveStats right)
+        {
+            return !left.Equals(right);
+        }
+    }
+}
