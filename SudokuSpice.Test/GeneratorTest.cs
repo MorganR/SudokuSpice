a85afb327e0d754e40d5c9f9604ebbd42990377d
Morgan Roff <roffmorgan@gmail.com>
2020-03-21 22:08:09 -0700

    Create Generator for generating puzzles

---
+++ SudokuSpice.Test/GeneratorTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Xunit;
+
+namespace SudokuSpice.Test
+{
+    public class GeneratorTest
+    {
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(3)]
+        [InlineData(30)]
+        [InlineData(36)]
+        [InlineData(400)]
+        public void Constructor_WithInvalidArgs_Throws(int size)
+        {
+            Assert.Throws<ArgumentException>(() => new Generator(size));
+        }
+
+        [Theory]
+        [InlineData(1, -1)]
+        [InlineData(1, 2)]
+        [InlineData(4, 0)]
+        [InlineData(4, 5)]
+        [InlineData(9, 16)]
+        [InlineData(9, 82)]
+        [InlineData(16, 40)]
+        [InlineData(16, 266)]
+        [InlineData(25, 100)]
+        [InlineData(25, 626)]
+        public void Generate_WithInvalidArgs_Throws(int size, int numToSet)
+        {
+            var generator = new Generator(size);
+
+            Assert.Throws<ArgumentException>(() => generator.Generate(numToSet));
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(4, 2)]
+        [InlineData(9, 30)]
+        public void Generate_CreatesPuzzleWithUniqueSolution(int size, int numToSet)
+        {
+            var generator = new Generator(size);
+            
+            var puzzle = generator.Generate(numToSet);
+
+            var solver = new Solver(puzzle);
+            var stats = solver.GetStatsForAllSolutions();
+            Assert.Equal(1, stats.NumSolutionsFound);
+        }
+    }
+}
---
+++ SudokuSpice/Generator.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SudokuSpice
+{
+    [SuppressMessage("Microsoft.Performance", "CA1814:PreferJaggedArraysOverMultidimensional")]
+    public class Generator
+    {
+        private const int MAX_ATTEMPTS = 100;
+        private readonly Random _random = new Random();
+        private readonly int _size;
+        private readonly int _boxSize;
+
+        public Generator(int size)
+        {
+            _size = size;
+            _boxSize = (int)Math.Sqrt(size);
+            if (_boxSize * _boxSize != size || _boxSize < 1 || _boxSize > 5)
+            {
+                throw new ArgumentException($"{nameof(size)} must be a perfect square in the range [1, 25].");
+            }
+        }
+
+        public Puzzle Generate(int numSquaresToSet)
+        {
+            _ValidateNumSquaresToSet(numSquaresToSet);
+            
+            Puzzle? puzzle = null;
+            for (int attempts = 0; puzzle is null && attempts < MAX_ATTEMPTS; attempts++)
+            {
+                puzzle = new Puzzle(new int?[_size, _size]);
+                _FillPuzzle(puzzle);
+                var setCoordinates = new CoordinateTracker(_size);
+                _TrackAllCoordinates(setCoordinates);
+                if (_TryUnsetSquaresWhileSolvable(numSquaresToSet, 0, setCoordinates, puzzle))
+                {
+                    break;
+                }
+                puzzle = null;
+            }
+            if (puzzle is null)
+            {
+                throw new TimeoutException($"Failed to generate a puzzle of size {_size} and {nameof(numSquaresToSet)} {numSquaresToSet} after {MAX_ATTEMPTS} attempts.");
+            }
+            return puzzle;
+        }
+
+        private void _ValidateNumSquaresToSet(int numToSet)
+        {
+            // Inclusive bounds
+            int lowerBound = 0;
+            int upperBound = 1;
+            switch (_boxSize)
+            {
+                case 1:
+                    break;
+                case 2:
+                    lowerBound = 1;
+                    upperBound = 4;
+                    break;
+                case 3:
+                    lowerBound = 17;
+                    upperBound = 81;
+                    break;
+                case 4:
+                    // Lower-bound estimate comes from
+                    // http://forum.enjoysudoku.com/minimum-givens-on-larger-puzzles-t4801.html
+                    lowerBound = 55;
+                    upperBound = 256;
+                    break;
+                case 5:
+                    // Lower-bound estimate comes from
+                    // http://forum.enjoysudoku.com/minimum-givens-on-larger-puzzles-t4801.html
+                    lowerBound = 185;
+                    upperBound = 625;
+                    break;
+            }
+            if (numToSet < lowerBound || numToSet > upperBound)
+            {
+                throw new ArgumentException(
+                    $"{nameof(numToSet)} must be in the range [{lowerBound}, {upperBound}] for puzzles of size {_size}.");
+            }
+        }
+
+        private void _FillPuzzle(Puzzle puzzle)
+        {
+            var solver = new Solver(puzzle);
+            solver.SolveRandomly();
+        }
+
+        private bool _TryUnsetSquaresWhileSolvable(int totalNumSquaresToUnset, int currentNumUnset, CoordinateTracker setCoordinatesToTry, Puzzle puzzle)
+        {
+            if (currentNumUnset == totalNumSquaresToUnset)
+            {
+                return true;
+            }
+            while (setCoordinatesToTry.NumTracked > 0)
+            {
+                var randomCoord = _GetRandomTrackedCoordinate(setCoordinatesToTry);
+                var previousValue = puzzle[in randomCoord];
+                setCoordinatesToTry.Untrack(in randomCoord);
+                if (!_TryUnsetSquareAt(randomCoord, puzzle))
+                {
+                    continue;
+                }
+                if (_TryUnsetSquaresWhileSolvable(totalNumSquaresToUnset, currentNumUnset + 1, new CoordinateTracker(setCoordinatesToTry), puzzle))
+                {
+                    return true;
+                }
+                // Child update failed :( replace value and try a different coordinate.
+                puzzle[in randomCoord] = previousValue;
+            }
+            return false;
+        }
+
+        private bool _TryUnsetSquareAt(in Coordinate c, Puzzle puzzle)
+        {
+            // Set without checks when there can't be conflicts.
+            if (puzzle.NumEmptySquares < 3)
+            {
+                puzzle[in c] = null;
+                return true;
+            }
+            var previousValue = puzzle[in c];
+            puzzle[in c] = null;
+            var solver = new Solver(new Puzzle(puzzle));
+            var solveStats = solver.GetStatsForAllSolutions();
+            if (solveStats.NumSolutionsFound == 1)
+            {
+                return true;
+            }
+            puzzle[in c] = previousValue;
+            return false;
+        }
+
+        private Coordinate _GetRandomTrackedCoordinate(CoordinateTracker tracker)
+        {
+            return tracker.GetTrackedCoords()[_random.Next(0, tracker.NumTracked)];
+        }
+
+        private void _TrackAllCoordinates(CoordinateTracker tracker)
+        {
+            for (int row = 0; row < _size; row++)
+            {
+                for (int col = 0; col < _size; col++)
+                {
+                    tracker.Add(new Coordinate(row, col));
+                }
+            }
+        }
+    }
+}
