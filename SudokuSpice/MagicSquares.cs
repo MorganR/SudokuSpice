5da625e4520a2b6d2cd6633a8e8ff1b04f1c4cd5
Morgan Roff <roffmorgan@gmail.com>
2021-09-19 16:47:21 +0200

    Add documentation to MagicSquares

--- SudokuSpice/MagicSquares.cs
+++ SudokuSpice/MagicSquares.cs
@@ -5,14 +5,38 @@ using System.Linq;
 
 namespace SudokuSpice
 {
+    /// <summary>
+    /// Utility methods for working with
+    /// <a href="https://en.wikipedia.org/wiki/Magic_square">magic squares</a>, i.e. the sums of the
+    /// values in each of their rows, columns, and (optionally) their diagonals add up to the same
+    /// value.
+    ///
+    /// These all require the possible values in the magic square to be unique.
+    /// </summary>
     public static class MagicSquares
     {
+        /// <summary>
+        /// Computes the "sum" that each row/column/diagonal must add up to.
+        /// </summary>
+        /// <param name="values">
+        /// All the possible values for the magic square. Each value must be unique.
+        /// </param>.
+        /// <exception cref="ArgumentException">
+        /// Thrown if no sum is possible.
+        /// </exception>
         public static int ComputeSum(ReadOnlySpan<int> values)
         {
             int boxSize = Boxes.IntSquareRoot(values.Length);
             return _ComputeSum(values, boxSize);
         }
 
+        /// <summary>
+        /// Computes each unique set of values of length SquareRoot(values.Length) that can be
+        /// formed from the given values.
+        /// </summary>
+        /// <param name="values">
+        /// All the possible values for the magic square. Each value must be unique.
+        /// </param>
         public static HashSet<BitVector> ComputeSets(ReadOnlySpan<int> values)
         {
             int boxSize = Boxes.IntSquareRoot(values.Length);
@@ -28,13 +52,29 @@ namespace SudokuSpice
             return ComputeSets(values, boxSize, allPossibleValues);
         }
 
+        /// <summary>
+        /// Computes each unique set of values of length <paramref name="boxSize"/> that can be
+        /// formed from the given values.
+        /// </summary>
+        /// <param name="values">
+        /// All the possible values for the magic square. Each value must be unique. Values must
+        /// have length `boxSize*boxSize`.
+        /// </param>
+        /// <param name="allPossibleValues">
+        /// The 
+        /// </param>
         public static HashSet<BitVector> ComputeSets(ReadOnlySpan<int> values, int boxSize, BitVector allPossibleValues)
         {
-            Debug.Assert(Boxes.TryIntSquareRoot(values.Length, out int expectedBoxSize)
-                && expectedBoxSize == boxSize,
-                $"Expected box size of {expectedBoxSize} for given size, but received {boxSize}.");
-            Debug.Assert(allPossibleValues.ComputeCount() == values.Length,
-                $"Expected {nameof(allPossibleValues)} to have the same length as {nameof(values)}.");
+            if (values.Length != boxSize * boxSize)
+            {
+                throw new ArgumentException(
+                    $"{nameof(boxSize)} ({boxSize}) must be the exact square root of the length of {nameof(values)} ({values.Length}).");
+            }
+            if (allPossibleValues.ComputeCount() != values.Length)
+            {
+                throw new ArgumentException($"Expected {nameof(allPossibleValues)} to have the same length as {nameof(values)}.");
+            }
+                
             int magicSum = _ComputeSum(values, boxSize);
             var sets = new HashSet<BitVector>();
             for (int i = 0; i < values.Length; ++i)
